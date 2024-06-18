643cb93c807e4b4e75ab4525d630a2a0e1430c5f
aryanraj13 <123410989+aryanraj13@users.noreply.github.com>
2024-06-19 02:41:33 +0530

    Designed category pages

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar/>
+      
       <div>
         <img src="/home(1).jpg" alt=""/>
       </div>
@@ -94,7 +94,7 @@ export default function Home() {
     </div>
   </div>
 </section>
-      <Footer/>
+ 
     </>
   );
 }
---
+++ pages/stickers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const stickers = () => {
+  return (
+    <div>
+      Stickers
+    </div>
+  )
+}
+
+export default stickers
