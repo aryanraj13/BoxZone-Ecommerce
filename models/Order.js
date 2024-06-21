2d38b05bb1163ebb2fffd983bf3f42fa58c25297
aryanraj13 <123410989+aryanraj13@users.noreply.github.com>
2024-06-22 02:05:54 +0530

    Connetion to mongoose

---
+++ models/Order.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    userId:{
+        type:String,
+        required:true
+    },
+    products:[{
+        productId:{type:String},
+        quantity:{type:Number,default:1}
+    }],
+    address:{
+        type:String,
+        required:true
+    },
+    amount:{
+        type:Number,
+        required:true
+    },
+    status:{
+        type:String,
+        defaul:'Pending',
+        required:true
+    }
+},{timestamps:true});
+
+export default mongoose.model("Order",OrderSchema);
