de7f78f532ac2f3322de7e1f6667f49efa60ebe1
aryanraj13 <123410989+aryanraj13@users.noreply.github.com>
2024-06-23 16:57:35 +0530

    Added password encrytion

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,18 +1,21 @@
 import User from "@/models/User";
 import connectDb from "@/middleware/mongoose";
+import CryptoJS from 'crypto-js';
 
 const handler = async (req, res) => {
   if (req.method == "POST") {
     console.log(req.body);
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      if (req.body.email == user.email && req.body.password == user.password) {
-        res.status(200).json({ success: true, email: user.email, name: user.name });
-      }else{
-        res.status(400).json({success:false,"error":"Invalid Credentials"});
-      }
-    }else{
-        res.status(400).json({success:false,"error":"User not found"});
+    let user = await User.findOne({ "email": req.body.email });
+    if (!user) {
+      res.status(400).json({success:false, "error":"User not found"});
+      return; // Stop further execution if no user is found
+    }
+    const bytes = CryptoJS.AES.decrypt(user.password, "secret123");
+    let decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
+    if (req.body.email == user.email && req.body.password == decryptedPass) {
+      res.status(200).json({ success: true, email: user.email, name: user.name });
+    } else {
+      res.status(400).json({success:false, "error":"Invalid Credentials"});
     }
   } else {
     res.status(400).json({ error: "This method is not allowed" });
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,9 +1,11 @@
+import CryptoJS from 'crypto-js'; // Import CryptoJS
 import User from "@/models/User";
 import connectDb from "@/middleware/mongoose";
 
 const handler=async (req,res)=>{
     if(req.method=='POST'){
-         let u = new User(req.body);
+        const {name,email} = req.body;
+         let u = new User({name,email,password:CryptoJS.AES.encrypt(req.body.password,"secret123").toString()});
          await u.save()
         res.status(200).json({success:"success"});
     }else{
@@ -12,7 +14,3 @@ const handler=async (req,res)=>{
 }
 
 export default connectDb(handler);
-
-
-
-  
